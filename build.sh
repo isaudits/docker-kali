579682b0e7bd3ec2944c24a09415dfe17dbb5160
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2018-09-15 12:45:50 -0400

    change no-cache

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull kalilinux/kali-linux-docker
-#docker build -t kali .
-docker build --no-cache -t kali .
+docker build -t kali .
+#docker build --no-cache -t kali .
 docker image prune -f
